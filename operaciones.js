aef4977e5355bf5d70facc789e97749624694896
Daniel Hernández Fajardo <alu0101320489@ull.edu.es>
2022-10-19 12:19:16 +0100

    NO FUNCA

--- operaciones.js
+++ operaciones.js
@@ -1,3 +1,9 @@
 function readMatrix() {
-    
+    let Ruta = document.getElementById("ruta").value;
+    console.log(Ruta);
 }
+
+
+$(document).ready(function(){
+    $('select').formSelect();
+  });
