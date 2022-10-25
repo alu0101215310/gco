0a9638893de43f58feb382f0a46bdb45319fc0ae
Espinette <alu0101235929@ull.edu.es>
2022-10-25 17:46:36 +0100

    Leer fichero e introducirselo a una variable
    Cambie un poco el estilo del html, le añadí bootstrap. Y en el js cree el evento que lee el fichero e introduce los datos en una matriz

--- operaciones.js
+++ operaciones.js
@@ -1,9 +1,31 @@
-function readMatrix() {
-    let Ruta = document.getElementById("ruta").value;
-    console.log(Ruta);
+// Obtenemos el fichero
+var fichero = document.getElementById('ruta')
+//Creamos la variable matriz principal para almacenar la matriz leida por fichero
+var matriz = []
+
+fichero.addEventListener('change', function(e) {
+    matriz = []
+    let reader = new FileReader()
+    reader.onload = function () {
+      let lines = reader.result.toString()
+      let filas = lines.split("\n")
+
+      //Insertamos los valores del fichero en la variable matriz
+      filas.forEach((fila) => {
+        matriz.push(fila.split(" "))
+      })
+    }
+    reader.readAsText(fichero.files[0])
+  }, false)
+  
+
+// Función principal que muestra el sistema de recomendación escogido
+function main() {
+//Comprobamos si la matriz se ha recogido del fichero correctamente
+  if (matriz.length == 0) { 
+    alert("No ha sido posible procesar la matriz")
+    throw new Error()
+  } else {
+    console.log(matriz)
+  }
 }
-
-
-$(document).ready(function(){
-    $('select').formSelect();
-  });
