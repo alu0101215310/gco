028178d46b6a7395fa0e7d9175c6089bf8c015cb
Espinette <alu0101235929@ull.edu.es>
2022-10-27 15:34:28 +0100

    Creada función para calcular la media aritmetica de un usuario

--- operaciones.js
+++ operaciones.js
@@ -4,21 +4,40 @@ var fichero = document.getElementById('ruta')
 var matriz = []
 
 fichero.addEventListener('change', function(e) {
-    matriz = []
-    let reader = new FileReader()
-    reader.onload = function () {
-      let lines = reader.result.toString()
-      let filas = lines.split("\n")
+  matriz = []
+  let reader = new FileReader()
+  reader.onload = function () {
+    let lines = reader.result.toString()
+    let filas = lines.split("\n")
 
-      //Insertamos los valores del fichero en la variable matriz
-      filas.forEach((fila) => {
-        matriz.push(fila.split(" "))
-      })
+    //Insertamos los valores del fichero en la variable matriz
+    filas.forEach((fila) => {
+      matriz.push(fila.split(" "))
+    })
+  }
+  reader.readAsText(fichero.files[0])
+}, false)
+
+// Calculamos la media de cada fila
+function calcular_media(matriz) {
+  //Vector donde almacenaremos los resultados
+  let medias = []
+  //Recorremos la matriz y calculamos la media
+  for (let i = 0; i < matriz.length; i++) {
+    let cont = 0
+    let sum = 0
+    for (let j = 0; j < matriz[i].length; j++) {
+      if (matriz[i][j] != "-") {
+        console.log(matriz[i][j])
+        sum = sum + parseInt(matriz[i][j])
+        cont++
+      } 
     }
-    reader.readAsText(fichero.files[0])
-  }, false)
+    medias.push(sum/cont)
+  }
+  return medias
+}
   
-
 // Función principal que muestra el sistema de recomendación escogido
 function main() {
 //Comprobamos si la matriz se ha recogido del fichero correctamente
@@ -26,6 +45,6 @@ function main() {
     alert("No ha sido posible procesar la matriz")
     throw new Error()
   } else {
-    console.log(matriz)
+    console.log(calcular_media(matriz))
   }
 }
