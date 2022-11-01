4f6e0e1352ef5d67e4830736bf4f58bafd66f834
Daniel Hernández Fajardo <alu0101320489@ull.edu.es>
2022-11-01 12:54:41 +0000

    distancia euclidea y obtener los valores de las métricas

--- operaciones.js
+++ operaciones.js
@@ -1,5 +1,6 @@
 // Obtenemos el fichero
 var fichero = document.getElementById('ruta')
+
 //Creamos la variable matriz principal para almacenar la matriz leida por fichero
 var matriz = []
 //Vector donde se almacenan las posiciones faltantes
@@ -30,7 +31,7 @@ function calcular_media(matriz) {
       if (matriz[i][j].trim() == '-') {
         
         if (!faltantes.includes(j)) {
-          faltantes.push(j);
+          faltantes.push(j)
         }
       }
     }
@@ -50,38 +51,46 @@ function calcular_media(matriz) {
   }
   return medias
 }
-
+//Función que calcula la correlación de Pearson
 function correlacion_pearson(matriz, usu1, usu2) {
-  let media = calcular_media(matriz);
-  let aux = 0;
-  let aux2 = 0;
-  let aux3 = 0;
+  let media = calcular_media(matriz)
+  let aux = 0
+  let aux2 = 0
+  let aux3 = 0
   for (let j = 0; j < matriz[usu1].length; j++) {
     if (!faltantes.includes(j)) {
-      aux = aux + ((matriz[usu1][j] - media[usu1])*(matriz[usu2][j] - media[usu2]));
-      aux2 = aux2 + (Math.pow(matriz[usu1][j] - media[usu1],2));
-      aux3 = aux3 + (Math.pow(matriz[usu2][j] - media[usu2],2));
+      aux = aux + ((matriz[usu1][j] - media[usu1])*(matriz[usu2][j] - media[usu2]))
+      aux2 = aux2 + (Math.pow(matriz[usu1][j] - media[usu1],2))
+      aux3 = aux3 + (Math.pow(matriz[usu2][j] - media[usu2],2))
     }
   }
-  aux2 = Math.sqrt(aux2) * Math.sqrt(aux3);
-  return aux/aux2;
+  aux2 = Math.sqrt(aux2) * Math.sqrt(aux3)
+  return aux/aux2
 }
+//Función que calcula la distancia coseno
 function distancia_coseno(matriz, usu1, usu2) {
-  let aux = 0;
-  let aux2 = 0;
-  let aux3 = 0;
+  let aux = 0
+  let aux2 = 0
+  let aux3 = 0
   for (let j = 0; j < matriz[usu1].length; j++) {
     if (!faltantes.includes(j)) {
-      aux = aux + ((matriz[usu1][j])*(matriz[usu2][j]));
-      aux2 = aux2 + (Math.pow(matriz[usu1][j],2));
-      aux3 = aux3 + (Math.pow(matriz[usu2][j],2));
+      aux = aux + ((matriz[usu1][j])*(matriz[usu2][j]))
+      aux2 = aux2 + (Math.pow(matriz[usu1][j],2))
+      aux3 = aux3 + (Math.pow(matriz[usu2][j],2))
     }
   }
-  aux2 = Math.sqrt(aux2) * Math.sqrt(aux3);
-  return aux/aux2;
+  aux2 = Math.sqrt(aux2) * Math.sqrt(aux3)
+  return aux/aux2
 }
-function distancia_euclidea(matriz) {
-
+//Función que calcula la distancia euclidea
+function distancia_euclidea(matriz, usu1, usu2) {
+  let aux = 0;
+  for (let j = 0; j < matriz[usu1].length; j++) {
+    if (!faltantes.includes(j)) {
+      aux = aux + Math.pow(matriz[usu1][j] - matriz[usu2][j], 2)
+    }
+  }
+  return Math.sqrt(aux)
 }
   
 // Función principal que muestra el sistema de recomendación escogido
@@ -91,8 +100,18 @@ function main() {
     alert("No ha sido posible procesar la matriz")
     throw new Error()
   } else {
+    let metrica = document.getElementById('metrica').value
     console.log(calcular_media(matriz))
-    console.log(correlacion_pearson(matriz,0,1))
-    console.log(distancia_coseno(matriz,0,1))
+    switch (metrica) {
+      case '1':
+        console.log(correlacion_pearson(matriz,0,1))
+        break;
+      case '2':
+        console.log(distancia_coseno(matriz,0,1))
+        break;
+      case '3':
+        console.log(distancia_euclidea(matriz,0,1))
+        break;
+    }
   }
 }
