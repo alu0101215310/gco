02e10c94174bfe8e72528394588196a183c8d8b1
Daniel Hernández Fajardo <alu0101320489@ull.edu.es>
2022-11-01 02:35:41 +0000

    correccion media, pearson y coseno

--- operaciones.js
+++ operaciones.js
@@ -2,6 +2,8 @@
 var fichero = document.getElementById('ruta')
 //Creamos la variable matriz principal para almacenar la matriz leida por fichero
 var matriz = []
+//Vector donde se almacenan las posiciones faltantes
+var faltantes = []
 
 fichero.addEventListener('change', function(e) {
   matriz = []
@@ -22,13 +24,24 @@ fichero.addEventListener('change', function(e) {
 function calcular_media(matriz) {
   //Vector donde almacenaremos los resultados
   let medias = []
+  //Recorremos la matriz encontrando todas las posiciones faltantes
+  for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+      if (matriz[i][j].trim() == '-') {
+        
+        if (!faltantes.includes(j)) {
+          faltantes.push(j);
+        }
+      }
+    }
+  }
+  console.log(faltantes);
   //Recorremos la matriz y calculamos la media
   for (let i = 0; i < matriz.length; i++) {
     let cont = 0
     let sum = 0
     for (let j = 0; j < matriz[i].length; j++) {
-      if (matriz[i][j] != "-") {
-        console.log(matriz[i][j])
+      if (!faltantes.includes(j)) {
         sum = sum + parseInt(matriz[i][j])
         cont++
       } 
@@ -41,22 +54,30 @@ function calcular_media(matriz) {
 function correlacion_pearson(matriz, usu1, usu2) {
   let media = calcular_media(matriz);
   let aux = 0;
+  let aux2 = 0;
+  let aux3 = 0;
   for (let j = 0; j < matriz[usu1].length; j++) {
-    if ((matriz[usu1][j] != "-")&&(matriz[usu2][j] != "-")) {
+    if (!faltantes.includes(j)) {
       aux = aux + ((matriz[usu1][j] - media[usu1])*(matriz[usu2][j] - media[usu2]));
-      aux2 = aux2 + ((Math.sqrt(matriz[usu1][j] - Math.pow(media[usu1],2))) * (Math.sqrt(Math.sqrt(matriz[usu1][j] - Math.pow(media[usu1],2)))));
+      aux2 = aux2 + (Math.pow(matriz[usu1][j] - media[usu1],2));
+      aux3 = aux3 + (Math.pow(matriz[usu2][j] - media[usu2],2));
     }
   }
+  aux2 = Math.sqrt(aux2) * Math.sqrt(aux3);
   return aux/aux2;
 }
-function distancia_coseno(matriz) {
+function distancia_coseno(matriz, usu1, usu2) {
   let aux = 0;
+  let aux2 = 0;
+  let aux3 = 0;
   for (let j = 0; j < matriz[usu1].length; j++) {
-    if ((matriz[usu1][j] != "-")&&(matriz[usu2][j] != "-")) {
+    if (!faltantes.includes(j)) {
       aux = aux + ((matriz[usu1][j])*(matriz[usu2][j]));
-      aux2 = aux2 + (Math.pow(matriz[usu1][j],2)) * (Math.sqrt(Math.pow(matriz[usu2][j],2)));
+      aux2 = aux2 + (Math.pow(matriz[usu1][j],2));
+      aux3 = aux3 + (Math.pow(matriz[usu2][j],2));
     }
   }
+  aux2 = Math.sqrt(aux2) * Math.sqrt(aux3);
   return aux/aux2;
 }
 function distancia_euclidea(matriz) {
@@ -71,5 +92,7 @@ function main() {
     throw new Error()
   } else {
     console.log(calcular_media(matriz))
+    console.log(correlacion_pearson(matriz,0,1))
+    console.log(distancia_coseno(matriz,0,1))
   }
 }
